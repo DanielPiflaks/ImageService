fe96de8820d4e2ad8387a887e051ca7909b0e1f6
Daniel Piflaks <Daniel Piflaks@DESKTOP-GOTQDME>
2018-04-10 20:31:04 +0300

    adding project files

---
+++ ImageService/ImageService/Controller/Handlers/DirectoyHandler.cs
@@ -0,0 +1,80 @@
+﻿using ImageService.Modal;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageService.Infrastructure;
+using ImageService.Infrastructure.Enums;
+using ImageService.Logging;
+using ImageService.Logging.Modal;
+using System.Text.RegularExpressions;
+using ImageService.Commands;
+
+namespace ImageService.Controller.Handlers
+{
+    public class DirectoyHandler : IDirectoryHandler
+    {
+        #region Members
+        private readonly string[] m_filesExtention = { "*.jpg", "*.png", "*.gif", "*.bmp" };
+        private IImageController m_controller;              // The Image Processing Controller
+        private ILoggingService m_logging;
+        private FileSystemWatcher m_dirWatcher;             // The Watcher of the Dir
+        private string m_path;                              // The Path of directory
+        #endregion
+
+        public event EventHandler<DirectoryCloseEventArgs> DirectoryClose;              // The Event That Notifies that the Directory is being closed
+
+        public DirectoyHandler(ImageController imageController, ILoggingService loggingService,
+            string path)
+        {
+            m_controller = imageController;
+            m_logging = loggingService;
+            m_path = path;
+            m_dirWatcher = new FileSystemWatcher(path);
+        }
+
+        public void StartHandleDirectory(string dirPath)
+        {
+            m_dirWatcher.Created += new FileSystemEventHandler(NewFileHandler);
+            m_dirWatcher.Changed += new FileSystemEventHandler(NewFileHandler);
+            m_dirWatcher.EnableRaisingEvents = true;
+
+            string[] files = Directory.GetFiles(dirPath);
+
+            foreach (var file in files)
+            {
+                string fileExtension = Path.GetExtension(file);
+                if (m_filesExtention.Contains(fileExtension))
+                {
+                    string[] args = { file };
+                    CommandRecievedEventArgs newCommand = new CommandRecievedEventArgs((int)CommandEnum.NewFileCommand,
+                        args, file);
+                    OnCommandRecieved(this, newCommand);
+                }
+            }
+        }
+
+        public void OnCommandRecieved(object sender, CommandRecievedEventArgs e)
+        {
+            bool result;
+            string output = m_controller.ExecuteCommand(e.CommandID, e.Args, out result);
+        }
+
+
+        public void NewFileHandler(object source, FileSystemEventArgs e)
+        {
+            string file = e.FullPath;
+            string fileExtension = Path.GetExtension(file);
+
+            if (m_filesExtention.Contains(fileExtension))
+            {
+                string[] args = { file };
+                CommandRecievedEventArgs newCommand = new CommandRecievedEventArgs((int)CommandEnum.NewFileCommand,
+                    args, file);
+                OnCommandRecieved(this, newCommand);
+            }
+        }
+    }
+}
---
+++ ImageService/ImageService/Server/ImageServer.cs
@@ -0,0 +1,55 @@
+﻿using ImageService.Controller;
+using ImageService.Controller.Handlers;
+using ImageService.Infrastructure.Enums;
+using ImageService.Logging;
+using ImageService.Modal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService.Server
+{
+    public class ImageServer
+    {
+        #region Members
+        private IImageController m_controller;
+        private ILoggingService m_logging;
+        #endregion
+
+        #region Properties
+        public IImageController Controller
+        {
+            get
+            {
+                return m_controller;
+            }
+            set
+            {
+                m_controller = value;
+            }
+        }
+        public ILoggingService Logging
+        {
+            get
+            {
+                return m_logging;
+            }
+            set
+            {
+                m_logging = value;
+            }
+        }
+        public event EventHandler<CommandRecievedEventArgs> CommandRecieved;          // The event that notifies about a new Command being recieved
+        #endregion
+
+       public ImageServer(IImageController controller, ILoggingService logging)
+        {
+            Controller = controller;
+            Logging = logging;
+
+
+        }
+    }
+}
