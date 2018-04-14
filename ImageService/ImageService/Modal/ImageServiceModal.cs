fa807d9f70ea86f396ac41bf8b03a01f6794ed73
Daniel Piflaks <Daniel Piflaks@DESKTOP-GOTQDME>
2018-04-14 13:47:02 +0300

    commit

--- ImageService/ImageService/Modal/ImageServiceModal.cs
+++ ImageService/ImageService/Modal/ImageServiceModal.cs
@@ -159,7 +159,7 @@ namespace ImageService.Modal
                 {
                     Directory.CreateDirectory(path);
                 }
-                catch (Exception e)
+                catch (Exception)
                 {
                     throw new Exception("Unabled to create folder " + path);
                 }
