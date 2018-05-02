28d7cadcaf0698f716e59e4190553ca78c0b4792
sapir blutman <sapir.blutman@live.biu.ac.il>
2018-05-02 22:41:56 +0300

    making wpf for log screen

--- ImageServiceGUI/Models/LogModel.cs
+++ ImageServiceGUI/Models/LogModel.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace ImageServiceGUI.Models
 {
-    class LogModel
+    public class LogModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        private string m_type;
+        public string Type
+        {
+            get { return m_type; }
+            set
+            {
+                m_type = value;
+                OnPropertyChanged("Type");
+            }
+        }
+
+        private string m_msg;
+        public string Message
+        {
+            get { return m_msg; }
+            set
+            {
+                m_type = value;
+                OnPropertyChanged("Msg");
+            }
+        }
     }
 }
--- ImageServiceGUI/ViewModels/LogViewModel.cs
+++ ImageServiceGUI/ViewModels/LogViewModel.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ImageServiceGUI.Models;
 
 namespace ImageServiceGUI.ViewModels
 {
-    class LogViewModel
+    public class LogViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged(String a)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(a));
+            }
+        }
+
+        private LogModel logView;
+        public LogModel LogModel
+        {
+            get { return this.logView; }
+            set { this.logView = value; }
+        }
+
+        public LogViewModel()
+        {
+            this.LogModel = new LogModel();
+            LogModel.PropertyChanged += delegate (Object sender, PropertyChangedEventArgs type)
+            {
+                NotifyPropertyChanged(type.PropertyName);
+            };
+        }
+        
     }
 }
