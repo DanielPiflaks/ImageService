ea00048f43a83991ab48fc9e291533fa5e715522
Daniel Piflaks <Daniel Piflaks@DESKTOP-GOTQDME>
2018-05-04 15:46:54 +0300

    Adding communication

--- Communication/TCPServerChannel.cs
+++ Communication/TCPServerChannel.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using ImageService.Logging;
+using ImageService.Logging.Modal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Communication
@@ -9,6 +14,12 @@ namespace Communication
     public class TCPServerChannel
     {
         #region Properties
+        // Create a new Mutex. The creating thread does not own the mutex.
+        private static Mutex mut = new Mutex();
+        private static List<Socket> listOfSockets;
+        private static TcpListener Listener;
+        private static ILoggingService Logging;
+
         private int m_port;
         public int Port
         {
@@ -16,13 +27,43 @@ namespace Communication
             set { m_port = value; }
         }
 
-        private string m_ip;
-        public string IP
+        private IPAddress m_ip;
+        public IPAddress IP
         {
             get { return m_ip; }
             set { m_ip = value; }
         }
 
         #endregion
+
+        public TCPServerChannel(string ip, int port, ILoggingService logging)
+        {
+            IP = IPAddress.Parse(ip);
+            Port = port;
+            Logging = logging;
+
+            Listener = new TcpListener(IP, port);
+            //Write to log.
+            Logging.Log("Creating TCP Server channel", MessageTypeEnum.INFO);
+
+            listOfSockets = new List<Socket>();
+        }
+
+        public static void StartListening()
+        {
+            Listener.Start();
+            Logging.Log("TCP Starting to listen for clients", MessageTypeEnum.INFO);
+            Socket s = Listener.AcceptSocket();
+            byte[] data = new byte[100];
+            var k = s.Receive(data);
+
+            var t = new Thread(() => HandleClient.HandleClientRequest(k));
+            t.Start();
+        }
+
+        public static void Send(object message)
+        {
+
+        }
     }
 }
