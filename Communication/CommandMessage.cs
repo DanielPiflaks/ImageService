a066d90aedda83333dbfef6f52a6f1978814095e
sapir blutman <sapir.blutman@live.biu.ac.il>
2018-05-04 15:46:25 +0300

    commiting

---
+++ Communication/CommandMessage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageService.Infrastructure.Enums;
+
+namespace Communication
+{
+    class CommandMessage
+    {
+        private CommandEnum m_cmdEnum;
+
+        public CommandEnum CmdEnum
+        {
+            get { return m_cmdEnum; }
+            set { m_cmdEnum = value; }
+        }
+
+        private List<String> m_args;
+
+        public List<String> ArgsList
+        {
+            get { return m_args; }
+            set { m_args = value; }
+        }
+
+        public CommandMessage(List<String> args, CommandEnum commandEnum)
+        {
+            m_cmdEnum = commandEnum;
+            m_args = args;
+        }
+
+    }
+}
--- Communication/TCPClientChannel.cs
+++ Communication/TCPClientChannel.cs
@@ -1,12 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using ImageService.Infrastructure.Enums;
 
 namespace Communication
 {
     class TCPClientChannel
     {
+        public const string IP = "127.0.0.1";
+        public const int PORT = 8888;
+
+        //members.
+        private static TCPClientChannel clientTcp;
+        private TcpClient m_tcpClient;
+        private Stream stm;
+
+        /// <summary>
+        /// properties.
+        /// </summary>
+        public TcpClient TCPClient
+        {
+            get { return m_tcpClient; }
+            set { m_tcpClient = value; }
+        }
+
+        /// <summary>
+        /// constructor.
+        /// </summary>
+        private TCPClientChannel()
+        {
+            TCPClient = new TcpClient();
+            TCPClient.Connect(IP, PORT);
+            stm = TCPClient.GetStream();
+        }
+
+        public static TCPClientChannel GetTCPClientChannel()
+        {
+            try
+            {
+                if (clientTcp == null)
+                {
+                    clientTcp = new TCPClientChannel();
+                }
+                return clientTcp;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public void Send(CommandEnum cmd, String[] args)
+        {
+            
+        }
+
+        public static CommandMessage Serialize(object anySerializableObject)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                (new BinaryFormatter()).Serialize(memoryStream, anySerializableObject);
+                return new CommandMessage { Data = memoryStream.ToArray() };
+            }
+        }
     }
 }
